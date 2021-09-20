bc0423604943792ae6f432867b1ed55415fa3a1f
angelofallars <ba.fallaria@gmail.com>
2021-09-21 03:29:20 +0800

    Add user input

--- tictac.c
+++ tictac.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
+#define clear() printf("\033[H\033[J")
+
 /* A tic tac toe game made in C. */
 int main(int argc, char* argv[])
 {
     char board[3][3];
+    int user_input;
 
     // Initialize the tic-tac-toe board with empty spaces
     for (int i = 0; i < 3; i++) {
@@ -22,7 +25,14 @@ int main(int argc, char* argv[])
         printf("│ %c │ %c │ %c │\n", board[2][0], board[2][1], board[2][2]);
         printf("└───┴───┴───┘\n");
 
-        getchar();
+        printf("(x turn) Place your piece from 1-9!\n$ ");
+
+        do {
+            scanf("%i", &user_input);
+        }
+        while (user_input < 1 || user_input > 9);
+
+        board[user_input / 3][user_input % 3 - 1] = 'x';
     }
 
     return 0;
